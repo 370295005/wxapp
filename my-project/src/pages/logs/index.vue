fef72a9fbe845cfd88795c96c8d9151c4a824325
370295005 <370295005@qq.com>
2021-05-06 22:33:12 +0800

    fix

--- my-project/src/pages/logs/index.vue
+++ my-project/src/pages/logs/index.vue
@@ -27,13 +27,13 @@
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-import * as echarts from 'echarts/dist/echarts.min'
-import mpvueEcharts from 'mpvue-echarts'
-let chart = null
-let Chart = null
+import { mapState } from "vuex";
+import * as echarts from "echarts/dist/echarts.min";
+import mpvueEcharts from "mpvue-echarts";
+let chart = null;
+let Chart = null;
 export default {
-  data () {
+  data() {
     return {
       option: null,
       Option: null,
@@ -43,70 +43,70 @@ export default {
       maxhum: 0,
       minhum: 0,
       avgtemp: 0,
-      avghum: 0
-    }
+      avghum: 0,
+    };
   },
   components: {
-    mpvueEcharts
+    mpvueEcharts,
   },
   computed: {
     ...mapState({
       devicetempdata: (state) => state.devicetempdata,
       devicehumdata: (state) => state.devicehumdata,
-      currenttime: (state) => state.currenttime
-    })
+      currenttime: (state) => state.currenttime,
+    }),
   },
-  mounted () {
-    this.Init()
+  mounted() {
+    this.Init();
   },
   watch: {
-    devicetempdata () {
-      this.updateData()
+    devicetempdata() {
+      this.updateData();
     },
-    devicehumdata () {
-      this.updateData()
-    }
+    devicehumdata() {
+      this.updateData();
+    },
   },
   methods: {
-    // 温度统计
-    initChart () {
+    //温度统计
+    initChart() {
       this.option = {
-        backgroundColor: '#fff',
-        color: ['#ff3333'],
+        backgroundColor: "#fff",
+        color: ["#ff3333"],
         title: {
-          text: '温度统计'
+          text: "温度统计",
         },
         tooltip: {
-          trigger: 'axis'
+          trigger: "axis",
         },
         grid: {
           containLabel: false,
           tooltip: {
             show: true,
-            trigger: 'axis'
-          }
+            trigger: "axis",
+          },
         },
         legend: {
-          type: 'plain',
+          type: "plain",
           show: true,
-          left: 'center',
-          top: 'top'
+          left: "center",
+          top: "top",
         },
         xAxis: {
-          type: 'category',
-          name: 'time',
-          nameLocation: 'end',
+          type: "category",
+          name: "time",
+          nameLocation: "end",
           nameGap: 5,
           // data: this.currentsec + "分" + this.currentmin + '秒',
-          data: this.currenttime
+          data: this.currenttime,
           // data: [24, 25, 19, 29, 25, 28, 20, 25, 26, 22, 21, 25, 29],
         },
         yAxis: [
           {
             splitLine: {
               lineStyle: {
-                type: 'dashed'
-              }
+                type: "dashed",
+              },
             },
             min:28,
             max:32,
@@ -114,149 +114,149 @@ export default {
             nameLocation: "end",
           },
         ],
-        // x轴可滑动
+        //x轴可滑动
         dataZoom: [
           {
-            start: 50,
-            end: 100,
-            type: 'inside'
-          }
+            start: 0,
+            end: 20,
+            type: "inside",
+          },
         ],
         series: [
           {
-            name: '温度',
-            type: 'line',
-            smooth: false, // 是否平滑显示曲线
+            name: "温度",
+            type: "line",
+            smooth: false, //是否平滑显示曲线
             label: {
-              // 是否显示标签
+              //是否显示标签
               normal: {
-                show: true
-              }
+                show: true,
+              },
             },
-            data: this.devicetempdata
+            data: this.devicetempdata,
             // data: [24, 25, 19, 29, 25, 28, 20, 25, 26, 22, 21, 25, 29],
-          }
-        ]
-      }
-      this.$refs.echarts.init()
+          },
+        ],
+      };
+      this.$refs.echarts.init();
     },
-    // 湿度统计
-    initchart () {
+    //湿度统计
+    initchart() {
       this.Option = {
-        backgroundColor: '#fff',
-        color: ['#3d7ef9'],
+        backgroundColor: "#fff",
+        color: ["#3d7ef9"],
         title: {
-          text: '湿度统计'
+          text: "湿度统计",
         },
         tooltip: {
-          trigger: 'axis'
+          trigger: "axis",
         },
         grid: {
           containLabel: false,
           tooltip: {
             show: true,
-            trigger: 'axis'
-          }
+            trigger: "axis",
+          },
         },
         legend: {
-          type: 'plain',
+          type: "plain",
           show: true,
-          left: 'center',
-          top: 'top'
+          left: "center",
+          top: "top",
         },
         xAxis: {
-          type: 'category',
-          name: 'time',
-          nameLocation: 'end',
+          type: "category",
+          name: "time",
+          nameLocation: "end",
           nameGap: 5,
-          data: this.currenttime
+          data: this.currenttime,
           // data: [24, 25, 19, 29, 25, 28, 20, 25, 26, 22, 21, 25, 29],
         },
         yAxis: [
           {
-            type: 'value',
+            type: "value",
             splitLine: {
               lineStyle: {
-                type: 'dashed'
-              }
+                type: "dashed",
+              },
             },
-            max:60,
+            max:80,
             min:50,
             name: "Hum",
             nameLocation: "end",
           },
         ],
-        // x轴可滑动
+        //x轴可滑动
         dataZoom: [
           {
-            start: 50,
-            end: 100,
-            type: 'inside'
-          }
+            start: 0,
+            end: 20,
+            type: "inside",
+          },
         ],
         series: [
           {
-            name: '湿度',
-            type: 'line',
+            name: "湿度",
+            type: "line",
             smooth: false,
             label: {
               normal: {
-                show: true
-              }
+                show: true,
+              },
             },
             // data: [24, 25, 19, 29, 25, 28, 20, 25, 26, 22, 21, 25, 29],
-            data: this.devicehumdata
-          }
-        ]
-      }
-      this.$refs.Echarts.init()
+            data: this.devicehumdata,
+          },
+        ],
+      };
+      this.$refs.Echarts.init();
     },
-    // 初始化echarts
-    handleInit (canvas, width, height) {
+    //初始化echarts
+    handleInit(canvas, width, height) {
       chart = echarts.init(canvas, null, {
         width: width,
-        height: height
-      })
-      canvas.setChart(chart)
-      chart.setOption(this.option)
-      return chart
+        height: height,
+      });
+      canvas.setChart(chart);
+      chart.setOption(this.option);
+      return chart;
     },
-    HandleInit (canvas, width, height) {
+    HandleInit(canvas, width, height) {
       Chart = echarts.init(canvas, null, {
         width: width,
-        height: height
-      })
-      canvas.setChart(Chart)
-      Chart.setOption(this.Option)
-      return Chart
+        height: height,
+      });
+      canvas.setChart(Chart);
+      Chart.setOption(this.Option);
+      return Chart;
     },
-    // 数组求平均值
-    avg (arr) {
-      let len = arr.length
-      let sum = 0
+    //数组求平均值
+    avg(arr) {
+      let len = arr.length;
+      let sum = 0;
       for (let i = 0; i < len; i++) {
-        sum += arr[i]
+        sum += arr[i];
       }
-      return parseFloat(sum / len).toFixed(2)
+      return parseFloat(sum / len).toFixed(2);
     },
-    // 更新数据
-    updateData () {
-      chart.setOption(this.option)
-      Chart.setOption(this.Option)
-      this.maxtemp = Math.max.apply(null, this.devicetempdata)
-      this.mintemp = Math.min.apply(null, this.devicetempdata)
-      this.maxhum = Math.max.apply(null, this.devicehumdata)
-      this.minhum = Math.min.apply(null, this.devicehumdata)
-      this.avgtemp = this.avg(this.devicetempdata)
-      this.avghum = this.avg(this.devicehumdata)
+    //更新数据
+    updateData() {
+      chart.setOption(this.option);
+      Chart.setOption(this.Option);
+      this.maxtemp = Math.max.apply(null, this.devicetempdata);
+      this.mintemp = Math.min.apply(null, this.devicetempdata);
+      this.maxhum = Math.max.apply(null, this.devicehumdata);
+      this.minhum = Math.min.apply(null, this.devicehumdata);
+      this.avgtemp = this.avg(this.devicetempdata);
+      this.avghum = this.avg(this.devicehumdata);
     },
-    // 初始化
-    Init () {
-      this.initChart()
-      this.initchart()
-    }
-  }
-}
+    //初始化
+    Init() {
+      this.initChart();
+      this.initchart();
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
